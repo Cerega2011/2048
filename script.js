12bd6b72ccffdc66beb6642853779cd6ea51bea0
Cerega2011 <12345cerega@mail.ru>
2024-03-19 17:04:31 +0300

    add chooseGame

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ let newGameButtonWin = document.querySelector(".win__game-Button")
 let gridContainer = document.querySelector(".grid__container")
 let scoreText = document.querySelector(".score__text")
 let gameContainerWin = document.querySelector(".game__container-win")
+let chooseGame = document.querySelector(".choose__game")
 let gridCell
+let canUndo = true
 let touchStart = {
     x: null,
     y: null
 }
 
-let returnButton =document.querySelector('.return_button')
+let returnButton = document.querySelector('.return_button')
 let moveHistory = []
 let cells = []
 let score = 0
 
 function undoMove() {
-    if(moveHistory.length > 0){
+    
+    if (moveHistory.length > 0 && canUndo) {
+        canUndo = false
         let previousState = moveHistory.pop()
         cells.forEach((cell, index) => {
             cell.value = previousState[index]
@@ -25,25 +29,25 @@ function undoMove() {
 }
 
 function handleKeyUp(event) {
-    if (event.key === 'ArrowRight' || event.code === 'KeyD' ) {
+    if (event.key === 'ArrowRight' || event.code === 'KeyD') {
         moveRight();
-    } else if (event.key === 'ArrowLeft' || event.code === 'KeyA' ) {
+    } else if (event.key === 'ArrowLeft' || event.code === 'KeyA') {
         moveLeft()
-    } else if (event.key === 'ArrowDown'  || event.code === 'KeyS' ) {
+    } else if (event.key === 'ArrowDown' || event.code === 'KeyS') {
         moveDown()
-    } else if (event.key === 'ArrowUp'  || event.code === 'KeyW' ) {
+    } else if (event.key === 'ArrowUp' || event.code === 'KeyW') {
         moveUp()
     }
 }
 
 function checkForWin() {
-    for(let i = 0; i < cells.length; i++){
-        if(cells[i].value == 2048){
+    for (let i = 0; i < cells.length; i++) {
+        if (cells[i].value == 2048) {
             gameContainerWin.classList.add('game__container-win__active')
             document.removeEventListener('keyup', handleKeyUp)
 
         }
-        
+
     }
 }
 
@@ -62,25 +66,40 @@ function createGrid() {
     updateBoard()
 }
 
-
+function resizeGrid(size) {
+    gridContainer.style.gridTemplateColumns = 'repeat(6, 100px)'
+    gridContainer.style.gridTemplateRows = 'repeat(6, 100px)'
+    gridContainer.innerHTML = ''
+    cells = []
+    for(let i = 0; i < size; i++){
+        gridCell = document.createElement('div');
+        gridCell.classList.add('grid-cell');
+        gridCell.innerHTML = 0;
+        gridContainer.appendChild(gridCell);
+        cells.push({ element: gridCell, value: 0 });
+    }
+    addNumber();
+    addNumber();
+    updateBoard()
+}
 
 function addNumber() {
 
-        let available = cells.filter(cell => cell.value === 0);
+    let available = cells.filter(cell => cell.value === 0);
     if (available.length > 0) {
         let randomCell = available[Math.floor(Math.random() * available.length)];
         randomCell.value = Math.random() > 0.9 ? 4 : 2;
         randomCell.element.innerHTML = randomCell.value;
-        if(randomCell.value == 2){
+        if (randomCell.value == 2) {
             randomCell.element.classList.add('grid-cell-2')
 
         }
-        if(randomCell.value == 4){
+        if (randomCell.value == 4) {
             randomCell.element.classList.add('grid-cell-4')
 
         }
     }
-    
+
 }
 
 function slideRight(row) {
@@ -105,33 +124,37 @@ function combineRight(row) {
 }
 
 function moveRight() {
-    let currentState= cells.map(cell => cell.value)
-    moveHistory.push(currentState) 
+    let currentState = cells.map(cell => cell.value)
+    moveHistory.push(currentState)
     for (let i = 0; i < 16; i += 4) {
-      let row = [
-        cells[i].value,
-        cells[i + 1].value,
-        cells[i + 2].value,
-        cells[i + 3].value,
-      ];
-      row = slideRight(row);
-      row = combineRight(row);
-      row = slideRight(row);
-      [
-        cells[i].value,
-        cells[i + 1].value,
-        cells[i + 2].value,
-        cells[i + 3].value,
-      ] = row;
+        let row = [
+            cells[i].value,
+            cells[i + 1].value,
+            cells[i + 2].value,
+            cells[i + 3].value,
+        ];
+        row = slideRight(row);
+        row = combineRight(row);
+        row = slideRight(row);
+        [
+            cells[i].value,
+            cells[i + 1].value,
+            cells[i + 2].value,
+            cells[i + 3].value,
+        ] = row;
     }
+    addNumber()
     updateBoard();
-  }
+    canUndo = true
+
+}
 
 
 
 function moveLeft() {
-    let currentState= cells.map(cell => cell.value)
-    moveHistory.push(currentState) 
+
+    let currentState = cells.map(cell => cell.value)
+    moveHistory.push(currentState)
     for (let i = 0; i < 16; i += 4) {
         let row = [cells[i].value, cells[i + 1].value, cells[i + 2].value, cells[i + 3].value]
         row = slideLeft(row)
@@ -139,8 +162,9 @@ function moveLeft() {
         row = slideLeft(row);
         [cells[i].value, cells[i + 1].value, cells[i + 2].value, cells[i + 3].value] = row;
     }
+    addNumber()
     updateBoard()
-    
+    canUndo = true
 }
 
 function newGame() {
@@ -150,35 +174,39 @@ function newGame() {
     updateScore();
     createGrid();
     gameContainerWin.classList.remove('game__container-win__active')
-    document.addEventListener('keyup',handleKeyUp);
+    document.addEventListener('keyup', handleKeyUp);
 }
 
 function moveUp() {
-    let currentState= cells.map(cell => cell.value)
-    moveHistory.push(currentState) 
-    for (let i = 0; i < 4; i++) {
-        let row = [cells[i].value, cells[i + 4].value, cells[i + 8].value, cells[i + 12].value]
+    let currentState = cells.map(cell => cell.value)
+    moveHistory.push(currentState)
+    for (let i = 0; i < 6; i++) {
+        let row = [cells[i].value, cells[i + 6].value, cells[i + 12].value, cells[i + 18].value, cells[i + 24].value, cells[i + 30].value]
         row = slideLeft(row)
         row = combineLeft(row);
         row = slideLeft(row);
-        [cells[i].value,cells[i + 4].value, cells[i + 8].value, cells[i + 12].value] = row;
-    }
+        [cells[i].value, cells[i + 6].value, cells[i + 12].value, cells[i + 18].value, cells[i + 24].value, cells[i + 30].value] = row;
+    } 
+    addNumber()
     updateBoard()
+    canUndo = true
 }
 
 
 
 function moveDown() {
-    let currentState= cells.map(cell => cell.value)
-    moveHistory.push(currentState) 
+    let currentState = cells.map(cell => cell.value)
+    moveHistory.push(currentState)
     for (let i = 0; i < 4; i++) {
         let row = [cells[i].value, cells[i + 4].value, cells[i + 8].value, cells[i + 12].value]
         row = slideRight(row)
         row = combineRight(row);
         row = slideRight(row);
-        [cells[i].value,cells[i + 4].value, cells[i + 8].value, cells[i + 12].value] = row;
-    }
+        [cells[i].value, cells[i + 4].value, cells[i + 8].value, cells[i + 12].value] = row;
+    } 
+    addNumber()
     updateBoard()
+    canUndo = true
 }
 
 function combineLeft(row) {
@@ -219,7 +247,7 @@ function updateBoard() {
             cell.element.classList.add('grid-cell-super');
         }
     }
-    addNumber();
+    // addNumber();
     updateScore();
     checkForWin()
 }
@@ -264,14 +292,14 @@ function updateScore() {
 
 createGrid();
 
-gridContainer.addEventListener('touchstart', function(event){
+gridContainer.addEventListener('touchstart', function (event) {
     touchStart.x = event.touches[0].clientX
     touchStart.y = event.touches[0].clientY
 })
 
 gridContainer.addEventListener('touchmove', function (event) {
-    if(!touchStart.x || !touchStart.y){
-   
+    if (!touchStart.x || !touchStart.y) {
+
         return
     }
     //let swipeInProgress = false
@@ -279,39 +307,44 @@ gridContainer.addEventListener('touchmove', function (event) {
     let touchEndY = event.touches[0].clientY
     let dx = touchEndX - touchStart.x
     let dy = touchEndY - touchStart.y
-    if(Math.abs(dx) > Math.abs(dy)){
-        if(dx > 0){
+    if (Math.abs(dx) > Math.abs(dy)) {
+        if (dx > 0) {
             moveRight()
             console.log('moveRight')
-        }else{
+        } else {
             moveLeft()
             console.log('moveLeft')
         }
-    }else{
-        if(dy > 0){
+    } else {
+        if (dy > 0) {
             moveDown()
             console.log('moveDown')
-        }else{
+        } else {
             moveUp()
             console.log('moveUp')
         }
     }
-  //  swipeInProgress = true
+    //  swipeInProgress = true
     touchStart.x = null
     touchStart.y = null
-  
+
 })
 
-gridContainer.addEventListener('touchend', function(){
-   // swipeInProgress = false
+gridContainer.addEventListener('touchend', function () {
+    // swipeInProgress = false
     touchStart.x = null
     touchStart.y = null
 })
 
-newGameButtonWin.addEventListener('click',newGame)
-newGameButton.addEventListener('click',newGame)
-document.addEventListener('keyup',handleKeyUp);
-returnButton.addEventListener('clcik', undoMove)
-
+newGameButtonWin.addEventListener('click', newGame)
+newGameButton.addEventListener('click', newGame)
+document.addEventListener('keyup', handleKeyUp);
+returnButton.addEventListener('click', function () {
+    undoMove()
+    console.log('returnButton')
+})
+chooseGame.addEventListener('click', function(){
+    resizeGrid(36)
+})
 
 // event.key == 'd' 
